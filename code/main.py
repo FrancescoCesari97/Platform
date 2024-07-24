4cd78e25dbb40345280ccfd78c0af83e00c2e91d
FrancescoCesari97 <freedus97@gmail.com>
2024-07-24 15:40:25 +0200

    added main map and decoration

--- code/main.py
+++ code/main.py
@@ -1,5 +1,5 @@
 from settings import * 
-
+from sprites import *
 class Game:
     def __init__(self):
         pygame.init()
@@ -8,9 +8,23 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = True
 
-        # groups 
+        # * groups 
         self.all_sprites = pygame.sprite.Group()
         self.collision_sprites = pygame.sprite.Group()
+
+        # *load game
+        self.setup()
+
+
+    
+    def setup(self):
+        tmx_map = load_pygame(join('data', 'maps', 'world.tmx'))
+
+        for x, y, image in tmx_map.get_layer_by_name('Main').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, (self.all_sprites, self.collision_sprites))
+        
+        for x, y, image in tmx_map.get_layer_by_name('Decoration').tiles():
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), image, self.all_sprites)
 
 
     def run(self):
